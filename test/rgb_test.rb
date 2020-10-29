f920dbd7f8b9a73375a0d6dc2d8e145ecb91b0c9
sho171 <sh.zxcv00@gmail.com>
2020-10-29 14:02:31 +0900

    Add: to_intsメソッド作成、テスト

--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -7,4 +7,11 @@ class RgbTest < Minitest::Test
     assert_equal '#ffffff', to_hex(255, 255, 255)
     assert_equal '#88296d', to_hex(136, 41, 109)
   end
+
+  def test_to_ints
+    assert_equal [0, 0, 0], to_ints('#000000')
+    assert_equal [255, 255, 255], to_ints('#ffffff')
+    assert_equal [24, 252, 31], to_ints('#18fc1f')
+  end
+
 end
